653fe86c242ecd421ee4ced79f6b0c15cffed7b2
nskadam02 <nkps1316@gmail.com>
2024-01-30 17:43:37 +0530

    default args

--- Scala_Basics/src/main/scala/Basics/defaultArg.scala
+++ Scala_Basics/src/main/scala/Basics/defaultArg.scala
@@ -1,11 +1,14 @@
 package Basics
 
+import scala.annotation.tailrec
+
 object defaultArg extends App{
 
 
   // deafult parameters should be at last
   // pass every lead argument
   // name args
+  @tailrec
   def trFact(n:Int ,acc:Int = 1): Int = {
     if (n<=1) acc
     else trFact(n-1,n*acc)
