930f9ab9ba4082bf1aa2b64ad112ce9677e72728
nskadam02 <nkps1316@gmail.com>
2024-02-05 12:01:05 +0530

    scala factory method

--- Scala_Basics/src/main/scala/part2/oop/Objects.scala
+++ Scala_Basics/src/main/scala/part2/oop/Objects.scala
@@ -10,11 +10,13 @@ object Objects extends App{
     // static / class level functionality
     val N_EYS = 2
     def canFly:Boolean = false
+
+    // factory method
+    def apply(mother:Person , father :Person):Person= new Person("Bobie")
   }
-  class Person{
+  class Person(val name:String){
     // instance level functionality
   }
-
   // COMPANIONS
 
 
@@ -22,8 +24,11 @@ object Objects extends App{
   println(Person.canFly)
 
   // Scala object = SINGLETON INSTANCE
-  val mary = Person
-  val john = Person
-  println(mary==john)
+  val mary = new Person("Mary")
+  val john = new Person("John")
+  val bobbie = Person(mary,john)
+
+  // Scala applictions = scala objects
+
 
 }
